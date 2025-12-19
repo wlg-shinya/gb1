4fe72317b8c8f3d12a88d596550892cf278a1ff7
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-12-19 15:05:11 +0900

    成果物をapp/outputに集約

--- app/build_run.bat
+++ app/build_run.bat
@@ -1,7 +1,8 @@
 REM Automatically generated from Makefile
-mkdir obj
-..\lib\gbdk\bin\lcc   -c -o obj\main.o src\main.c
-..\lib\gbdk\bin\lcc   -c -o obj\dungeon_map.o res\dungeon_map.c
-..\lib\gbdk\bin\lcc   -c -o obj\dungeon_tiles.o res\dungeon_tiles.c
-..\lib\gbdk\bin\lcc   -o obj\gb1.gb obj\main.o obj\dungeon_map.o obj\dungeon_tiles.o 
-..\lib\bgbw64\bgb64.exe obj\gb1.gb
+mkdir output\obj
+..\lib\gbdk\bin\lcc   -c -o output\obj\main.o src\main.c
+..\lib\gbdk\bin\lcc   -c -o output\obj\dungeon_map.o res\dungeon_map.c
+..\lib\gbdk\bin\lcc   -c -o output\obj\dungeon_tiles.o res\dungeon_tiles.c
+mkdir output\rom
+..\lib\gbdk\bin\lcc   -o output\rom\gb1.gb output\obj\main.o output\obj\dungeon_map.o output\obj\dungeon_tiles.o 
+..\lib\bgbw64\bgb64.exe output\rom\gb1.gb
