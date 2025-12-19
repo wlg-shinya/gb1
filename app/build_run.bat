0c1b5bb2c9098ea3cdcfd48e9daa775ff9543a72
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-12-19 19:15:13 +0900

    update build_run.bat

--- app/build_run.bat
+++ app/build_run.bat
@@ -1,8 +1,10 @@
 REM Automatically generated from Makefile
 mkdir output\obj
-..\lib\gbdk\bin\lcc   -c -o output\obj\main.o src\main.c
-..\lib\gbdk\bin\lcc   -c -o output\obj\dungeon_map.o res\dungeon_map.c
-..\lib\gbdk\bin\lcc   -c -o output\obj\dungeon_tiles.o res\dungeon_tiles.c
+..\lib\gbdk\bin\lcc -Wm-yc -Ires -c -o output\obj\dscan.o src\dscan.c
+..\lib\gbdk\bin\lcc -Wm-yc -Ires -c -o output\obj\bkg.o res\bkg.c
+..\lib\gbdk\bin\lcc -Wm-yc -Ires -c -o output\obj\bkg_c.o res\bkg_c.c
+..\lib\gbdk\bin\lcc -Wm-yc -Ires -c -o output\obj\bkg_m.o res\bkg_m.c
+..\lib\gbdk\bin\lcc -Wm-yc -Ires -c -o output\obj\fore.o res\fore.c
 mkdir output\rom
-..\lib\gbdk\bin\lcc   -o output\rom\gb1.gb output\obj\main.o output\obj\dungeon_map.o output\obj\dungeon_tiles.o 
-..\lib\bgbw64\bgb64.exe output\rom\gb1.gb
+..\lib\gbdk\bin\lcc -Wm-yc -Ires -o output\rom\gb1.gb output\obj\dscan.o output\obj\bkg.o output\obj\bkg_c.o output\obj\bkg_m.o output\obj\fore.o 
+..\lib\sameboy_winsdl\sameboy.exe output\rom\gb1.gb
